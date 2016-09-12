e71f57deebb77c0d9a93fbccd7c71e0f5c4e4999
Anderson Hung <anderson.hung@live.com>
2016-09-12 22:11:45 +0000

    images now save to avatar folder

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,11 @@
 var request = require('request');
+var fs = require('fs');
+var http = require('http');
 
+//var localPath = "/avatar";
 var owner = process.argv[2];
 var repo = process.argv[3];
-var avatarArray = [];
+//var avatarArray = [];
 var url = "https://api.github.com/repos/" + owner + "/" + repo + "/contributors";
 
 var options = {
@@ -17,15 +20,22 @@ var options = {
 
 function getRepoContributors(repoOwner, repoName, cb) {
   request(options, cb);
+
 }
 
 getRepoContributors(owner, repo, function(err,response,body){
   var data = JSON.parse(body);
   for (var i = 0; i < data.length; i++){
-    //console.log(data[i].avatar_url);
-    avatarArray.push(data[i].avatar_url);
-  }
-  console.log(avatarArray);
+
+  console.log(data);
+  var destination = fs.createWriteStream(`./avatar/${data[i].login}.jpg`);
+
+  request(data[i].avatar_url).pipe(destination);
+
+    }
 });
 
 
+/*function downloadImages(arr){
+  fs.writeFile()
+}*/
